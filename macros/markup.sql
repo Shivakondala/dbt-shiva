448108bccad44a24bfda399530c65afda47a6ee6
shiva kondala <shiva.kondala99@gmail.com>
2024-04-07 11:42:24 +0000

    made few changes added macros

---
+++ macros/markup.sql
@@ -0,0 +1,8 @@
+{%  macro markup(selling_price,COST_PRICE)  %}
+
+({{selling_price}}-{{COST_PRICE}})/{{COST_PRICE}}
+
+
+
+
+{%endmacro%}
--- models/staging/staging_orders.sql
+++ models/staging/staging_orders.sql
@@ -1,9 +1,12 @@
 select
 o.orderid,o.orderdate,o.shipdate,o.shipmode,o.selling_price,o.COST_PRICE,o.SELLING_PRICE-o.COST_PRICE AS PROFIT,
 c.customername,c.segment,c.country,c.customerid,
-p.productname,p.category,p.subcategory,p.productid
+p.productname,p.category,p.subcategory,p.productid,
+{{markup('selling_price','COST_PRICE')}} as markup
 FROM {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customers') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_products') }} as p
 on o.productid = p.productid
+
+
